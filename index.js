b4e4cfb2c8c2dba91099f3fd925f1df72d32e754
Kobe Fung <kobefungs@gmail.com>
2024-11-12 10:34:40 +0700

    submission

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
+
 function convertTimeToWords(time) {
   // TODO: real code goes here!
+  const words = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+    'twenty',
+    'twenty one',
+    'twenty two',
+    'twenty-three',
+    'twenty four',
+    'twenty five',
+    'twenty six',
+    'twenty seven',
+    'twenty eight',
+    'twenty nine',
+  ];
+
   if (time === '0:00') {
     return 'midnight';
   }
+  if (time === '12:00') {
+    return 'midday';
+  }
+  let splitted = time.split(':');
+  let hour = splitted[0];
+  let minutes = splitted[1];
 
-  return 'half past eight';
+  if (minutes === '0') {
+    return `${words[hour]} o'clock`;
+  } else if (minutes > 0 && minutes < 30) {
+    return `${words[minutes]} past ${words[hour]}`;
+  } else if (minutes === '30') {
+    return `half past ${words[hour]}`;
+  } else if (minutes === '45') {
+    return `quarter to ${words[hour + 1]}`;
+  } else if (minutes > 30 && minutes < 45) {
+    return `${words[60 - minutes]} to ${words[hour + 1]}`;
+  } else if (minutes > 45 && minutes < 60) {
+    return `${words[60 - minutes]} to ${words[hour + 1]}`;
+  }
 }
 
 module.exports = { convertTimeToWords };
--- readme.md
+++ readme.md
@@ -15,3 +15,15 @@ Write an function that converts time into the corresponding english words - eg. 
 - '2:40' > 'twenty to three'
 - '2:45' > 'quarter to three' 
 - '2:55' > 'five to three'
+
+
+/*
+
+// min = 00, hour= input hh
+// min > 0 but < 30, hour = input hh , min 
+// min = 30 half past , hour = input
+// min = 45 "quarter" to hour = +1
+// min > 45 < 60, min= 60-min , to hour = +1
+
+
+*/
