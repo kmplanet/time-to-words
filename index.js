484e0816ad4fe49e46f850ebe30f0dd2b42a2cc2
Kobe Fung <kobefungs@gmail.com>
2024-11-12 11:56:22 +0700

    refactored - convert variables from string to number and handle hour more than 13

--- index.js
+++ index.js
@@ -42,21 +42,21 @@ function convertTimeToWords(time) {
     return 'midday';
   }
   let splitted = time.split(':');
-  let hour = splitted[0];
-  let minutes = splitted[1];
+  let hour = Number(splitted[0])
+  let minutes = Number(splitted[1])
 
   if (minutes === '0') {
     return `${words[hour]} o'clock`;
   } else if (minutes > 0 && minutes < 30) {
     return `${words[minutes]} past ${words[hour]}`;
-  } else if (minutes === '30') {
+  } else if (minutes === 30) {
     return `half past ${words[hour]}`;
-  } else if (minutes === '45') {
+  } else if (minutes === 45) {
     return `quarter to ${words[hour + 1]}`;
   } else if (minutes > 30 && minutes < 45) {
-    return `${words[60 - minutes]} to ${words[hour + 1]}`;
+    return `${words[60 - minutes]} to ${words[hour%12 + 1]}`; 
   } else if (minutes > 45 && minutes < 60) {
-    return `${words[60 - minutes]} to ${words[hour + 1]}`;
+    return `${words[60 - minutes]} to ${words[hour%12 + 1]}`;
   }
 }
 
